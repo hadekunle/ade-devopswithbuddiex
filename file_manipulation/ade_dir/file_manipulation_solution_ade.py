48ac56e709ec432b700520e79e477e18224fcfdc
hadekunle <113251943+hadekunle@users.noreply.github.com>
2024-06-04 19:29:53 -0500

    ade soln file mani (#55)
    * file mani ade soln
    
    * file mani ade soln

---
+++ file_manipulation/ade_dir/file_manipulation_solution_ade.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+
+base_directory = os.path.dirname(os.path.abspath(__file__))
+curr_dir = f'{base_directory}/backup'
+target  = f'{base_directory}/backup2'
+os.makedirs(f'{target}/pictures/', exist_ok=True)
+
+def list_files(directory):
+    all_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            all_files.append(file)
+    return all_files
+
+list_dir_1 = list_files(curr_dir)
+
+all_txt=[]
+for each in list_dir_1:
+    if each.endswith('.jpg'):
+        src_1 = f'{curr_dir}/{each}'
+        target_1 = f'{target}/pictures/{each}'
+        shutil.copy(src_1, target_1)
+
+    elif each.endswith('.html'):
+        src_1 = f"{curr_dir}/{each}"
+        target_1 = f"{target}/{each.replace('.html','.htm')}"
+        shutil.copy(src_1, target_1)
+
+    elif each.endswith('.txt'):
+        all_txt.append(each)
+
+combined_txt = "stuff.txt"
+for each in sorted(all_txt):
+    os.system(f'cat {each} >> {combined_txt}')
+os.system(f'mv {combined_txt} {target}')
