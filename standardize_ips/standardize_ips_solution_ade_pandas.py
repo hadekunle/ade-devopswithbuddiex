2f60e1065320c263ef332ea0e6fffedc4f8e1507
hadekunle <shitoheeb@gmail.com>
2024-06-16 05:33:12 -0500

    ip solns

---
+++ standardize_ips/standardize_ips_solution_ade_pandas.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+import pandas as pd
+
+os.system('clear')
+directory = os.path.dirname(os.path.abspath(__file__))
+# directory = os.getcwd()
+path = f'{directory}/file'
+
+def clean_ip(x):
+    parts = re.split(r'[-.,\s]+',x)
+    part_clean = [str(int(x)) for x in parts]
+    combined = ".".join(part_clean)
+    return combined
+
+df = pd.read_csv(path,header=None,sep='@')
+df['clean'] = df[0].map(lambda x: clean_ip(x))
+
+clean_col = []
+for col in range(4):
+    new_col = f'col_{col}'
+    clean_col.append(new_col)
+    df[new_col] = df['clean'].map(lambda x: int(x.split(".")[col]))
+
+
+df = df.sort_values(by=[*clean_col],ascending=True)
+df = df.drop(columns=[0,*clean_col])
+df = df.reset_index()
+df.index += 1
+df = df.drop(columns=['index'])
+df = df.rename(columns={'clean': 'SORTED IP ADDRESSES'}) 
+print(df)
