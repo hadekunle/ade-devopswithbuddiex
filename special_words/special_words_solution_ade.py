f29a11ee7e6a8659929fdb318d8ca51923b79a65
hadekunle <shitoheeb@gmail.com>
2024-07-21 09:02:51 -0500

    updated soln and more test cases

--- special_words/special_words_solution_ade.py
+++ special_words/special_words_solution_ade.py
@@ -5,10 +5,13 @@ os.system('clear')
 directory = os.path.dirname(os.path.abspath(__file__))
 path = f'{directory}/file'
 
-pattern = r'^he[A-Za-z]*(er)|^her' 
+pattern = r'^he[a-z]*(er)$|^her[a-z]*$' 
 
+print('*'*30)
 with open (path,'r') as file:
     for line in file:
         line = line.strip()
-        if re.match(pattern,line):
-            print(line)
+        if re.match(pattern,line,re.IGNORECASE):
+            print(f'Matched  ✅: {line}')
+        else:
+            print(f'No Match ❌: {line}')
