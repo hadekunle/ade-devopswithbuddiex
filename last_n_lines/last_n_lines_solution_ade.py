964df99796ecc1781d240820d12f295b51358896
hadekunle <shitoheeb@gmail.com>
2024-08-14 18:07:20 -0500

    fixes for edge cases

--- last_n_lines/last_n_lines_solution_ade.py
+++ last_n_lines/last_n_lines_solution_ade.py
@@ -12,15 +12,20 @@ def print_n_lines(n_count):
     with open(path,'r') as file:
         content = file.readlines()
 
+        if len(content)== 0:
+            print('File is empty')
+            exit()
+
         if not 1 <= n_count <= len(content):
             print(f'Last N Count should be between 1 and {len(content)}')
             exit()
 
-        cleaned_content = [each.strip() for each in content[-n_count:] ]
+        cleaned_content = [each.strip() for each in content if each.strip() != '' ]
+        cleaned_content = cleaned_content[-n_count:]
     return cleaned_content
 
 def main():    
-    n_count = 2
+    n_count = 3
     result = print_n_lines(n_count)
     print(*result, sep='\n')
 
