939da45114287cb51c0e5ac67d2c0a6d24cc38f4
hadekunle <shitoheeb@gmail.com>
2024-08-29 12:12:12 -0500

    fix for % and whitedelim and 2 addn problems

--- sort_disk_space/sort_disk_space_solution_ade.py
+++ sort_disk_space/sort_disk_space_solution_ade.py
@@ -10,7 +10,8 @@ file2 = f'{base_directory}/file2'
 df1 = pd.read_csv(file1, delim_whitespace=True)
 df2 = pd.read_csv(file2, delim_whitespace=True)
 df = df1.set_index('Server').join(df2.set_index('Server'),how='inner')
-df = df.sort_values(by=['Space_used(TB)'], ascending = True)
+df['disk_usage%'] = round(df['Space_used(TB)'] / df['Total_space(TB)'] *100 ,2)
+df = df.sort_values(by=['disk_usage%'], ascending = True)
 df = df.reset_index()
 df.index += 1
 print(df)
