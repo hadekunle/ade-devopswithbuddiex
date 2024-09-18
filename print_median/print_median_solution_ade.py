199a06157348a27009f3eba45c389ddb14c8ca36
hadekunle <shitoheeb@gmail.com>
2024-09-17 19:55:22 -0500

    test push

--- print_median/print_median_solution_ade.py
+++ print_median/print_median_solution_ade.py
@@ -5,7 +5,7 @@ os.system('clear')
 directory = os.path.dirname(os.path.abspath(__file__))
 path = f'{directory}/file'
 path2 = f'{directory}/ade_cleaned_file'
-
+# this is a test
 def clean_file():
     with open (path2,'w') as cleaned_file:
         with open (path,'r') as file:
