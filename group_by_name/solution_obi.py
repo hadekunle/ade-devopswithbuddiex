8b848e30165a3df8009b80255d166be2714cc6d4
hadekunle <shitoheeb@gmail.com>
2024-10-26 14:46:34 -0500

    linting

--- group_by_name/solution_obi.py
+++ group_by_name/solution_obi.py
@@ -1,19 +1,5 @@
 import os
 def sort_by_groupname(input_file, output_file):
-    with open(input_file, "r") as file:
-        lines = file.readlines()
-
-
-    lines = [line.strip() for line in lines]
-
-   
-    sorted_lines = sorted(lines, key=lambda x: x.split()[1])
-
-    with open(output_file, "w") as file:
-        for line in sorted_lines:
-            file.write(line + "\n")
-
-def sort_by_groupname(input_file, output_file):
     with open(input_file, 'r') as file:
         lines = file.readlines()
     
@@ -25,9 +11,7 @@ def sort_by_groupname(input_file, output_file):
         for line in sorted_lines:
             file.write(line + '\n')
 
-
-
 if __name__ == "__main__":
     dir_a = "group_by_name/newfile2"
-    dir_b ="group_by_name/newfile2_soln"
-    soln(directory_path)
+    dir_b = "group_by_name/newfile2_soln"
+    # soln(directory_path)
