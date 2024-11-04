bbb164899a308071c9aaa0e49103f6bc4db69d05
hadekunle <shitoheeb@gmail.com>
2024-11-03 19:47:54 -0600

    pyspark

--- pyspark_10308.py
+++ pyspark_10308.py
@@ -1,15 +1,15 @@
-from pyspark.sql import functions as F
-df1 = db_employee
-df2 = db_dept
+# from pyspark.sql import functions as F
+# df1 = db_employee
+# df2 = db_dept
 
-df3 = df1.join(df2, df1['department_id'] == df2['id'], 'left')
-df4 = df3.drop(df2['id'])
-df5 = df4.groupBy('department').agg(
-    F.max('salary').alias('max_salary'),
-)
+# df3 = df1.join(df2, df1['department_id'] == df2['id'], 'left')
+# df4 = df3.drop(df2['id'])
+# df5 = df4.groupBy('department').agg(
+#     F.max('salary').alias('max_salary'),
+# )
 
-df5 = df5.toPandas()
+# df5 = df5.toPandas()
 
-max_salary_mkt = df5.loc[df5['department'] == 'marketing', 'max_salary'].values[0]
-max_salary_eng = df5.loc[df5['department'] == 'engineering', 'max_salary'].values[0]
-abs(max_salary_mkt - max_salary_eng)
+# max_salary_mkt = df5.loc[df5['department'] == 'marketing', 'max_salary'].values[0]
+# max_salary_eng = df5.loc[df5['department'] == 'engineering', 'max_salary'].values[0]
+# abs(max_salary_mkt - max_salary_eng)
